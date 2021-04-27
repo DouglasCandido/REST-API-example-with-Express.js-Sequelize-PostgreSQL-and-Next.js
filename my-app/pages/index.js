940244251f6c23c569e1a4e742ce11aac31a79e5
DouglasCandido <douglasmateus1@hotmail.com>
2021-04-27 02:10:57 -0300

    Last updates.

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,23 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useSWR from 'swr'
 
 export default function Carros() {
 
-    const [carros, setCarros]= useState('resgate')
     const {data, error} = useSWR(`http://localhost:8080/api/cars`, Fetcher)
 
     console.log(data)
 
     return (
 
-        <div>
             <div>
-              
-              {data ? <div> {data.map((c) => <div>{c.chassi} <br /> {c.model} <br /> {c.color} </div> )} </div>  : <p> Não foram encontrados carros. </p>}
+
+              <center> <h1> <strong> Exibir os dados dos carros que estão cadastrados no sistema </strong> </h1> </center>
+
+              <ul>
+
+                { 
+                
+                  data ? 
+                    
+                    data.map((c) => 
                       
+                      <li key={c.chassi}> Número do chassi: {c.chassi} <br /> Modelo: {c.model} <br /> Cor: {c.color} </li> 
+
+                    )
+      
+                    : <p> Nenhum carro ainda foi cadastrado no sistema. </p>
+
+                }
+
+              </ul>
+
+              <center> <h1> <strong> Cadastrar um novo carro </strong> </h1> </center>
+
+              <center> <h1> <strong> Alterar os dados de um carro que já está cadastrado </strong> </h1> </center>
+
+              <center> <h1> <strong> Excluir os dados de um carro que já está cadastrado </strong> </h1> </center>
+ 
             </div>
-            
-        </div>
 
     )
 
